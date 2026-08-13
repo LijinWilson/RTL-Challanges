d9299d8f7d4e2b2b81a3d9a43e2971bfe5ccad1a
LIJIN WILSON <112461401+LijinWilson@users.noreply.github.com>
2026-08-13 19:51:05 +0530

    Fix the code from Bug issue like zero input, dependency of data updation in reset signal

--- 2. GCD/GCD_EuclideanDIfference.v
+++ 2. GCD/GCD_EuclideanDIfference.v
@@ -1,37 +1,51 @@
 // GCD machine based on Eucilidian Difference Method
 
-module GCD_EucDiff(clk, rst, a, b, GCD);
+module GCD_EucDiff(clk, rst, load, a, b, GCD);
 
-input clk, rst;
+input clk, rst, load;
 input [7:0] a, b;
 output reg [7:0] GCD;
 
 reg [7:0] a_reg, b_reg;
 
 always @(posedge clk or posedge rst)
+// Inside the clocked always, we must use non blocking statements
  begin
+// To prevent from zero input(which cause the machine to run forever)
+if(a_reg == 0)
+    GCD <= b_reg;
+else if(b_reg == 0)
+    GCD <= a_reg;
+
   if(rst)
    begin
-    a_reg <= a;
-    b_reg <= b;
+    a_reg <= 0;
+    b_reg <= 0;
     GCD <= 0; 
    end
   else
    begin
-    if(a_reg == b_reg)
+    if(load) 
      begin
-      GCD = a_reg;
+      a_reg <= a;
+      b_reg <= b;
      end
-    else if(a_reg > b_reg)
+    else 
+     begin
+      if(a_reg == b_reg)
+       begin
+        GCD <= a_reg;
+       end
+      else if(a_reg > b_reg)
        begin
 	a_reg <= a_reg -  b_reg;
        end
       else
 	begin
-	 b_reg = b_reg - a_reg;
+	 b_reg <= b_reg - a_reg;
 	end
+end
      end
   end
-
 
 endmodule
